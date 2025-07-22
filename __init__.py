e655ac08cc72c4cba463856378a9869d4c7f8964
cras-1296 <caraya.salazar@gmail.com>
2025-07-21 22:48:52 -0400

    Primer commit: Sube el proyecto

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# Al inicio de test_reservas.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.reservas import verificar_disponibilidad
